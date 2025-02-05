7455a0d1ca786bd63af2f760fe0e9abf26d98ee9
unaisek <mohammedunaiseknkt@gmail.com>
2025-02-05 18:26:29 +0530

    Handling error page created

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,17 @@
+
+function getRandomInt(count:number){
+  return Math.floor(Math.random() * count)
+}
+
 export default async function ProductDetails({
   params,
 } : { 
     params: Promise< { productId: string}>;
 }) {
+  const random = getRandomInt(2);
+  if(random ==1) {
+    throw new Error("Error loadin product Details")
+  }
   const productId = (await params).productId
   return <h1>This is the Product{productId} Details</h1>
 }
