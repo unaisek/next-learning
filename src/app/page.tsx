bc6d2e5860af6153914cab2d1a336daaf8f6a5fc
unaisek <mohammedunaiseknkt@gmail.com>
2025-02-05 18:01:00 +0530

    Navigation through programmaticaly created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
+
 export default function Home () {
-  return <h1>Welocme Home</h1>;
+  return (
+    <>
+      <h1>Welcom Home!</h1>
+      <Link href="/profile">Profile</Link>
+    </>
+  );
 }
